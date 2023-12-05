cca2de85eca3291674d514d372796ec49a0b1baf
jayden <jeonghoochoe@gmail.com>
2023-12-05 15:25:28 +0000

    update WhatsOn page and Booking page

--- js/main.js
+++ js/main.js
@@ -35,19 +35,23 @@ $(function () {
 
 
 // JS for the BookTicket page 
-window.onload = () => {
+window.onload = function(){
     for(let i = 0; i < 8; i++){
         let date = new Date();
         date.setDate(date.getDate() + i);
         document.getElementById("select-day" + (i + 1) + "-z").innerHTML = date.toDateString().split(" ")[1] + " " + date.toDateString().split(" ")[2];
     }
-    // return true;
+    return true;
 }
 
 //seat click event
 const selectBox = document.querySelector(".select-zone-z");
 const pickedSeat = document.getElementById("picked-seat-z");
-const movieCard = document.getElementsByClassName("movie-card-z");
+const totalPrice = document.getElementById("total-price-z");
+const selectMovie = document.getElementById("select-movie-z");
+const picked=[];
+
+
 selectBox.addEventListener("click",
     e => {
         if (
@@ -57,21 +61,50 @@ selectBox.addEventListener("click",
             e.target.classList.toggle("seat-selected-z");
         }
 
-        const picked = [];
         let seats = document.getElementsByClassName("seat-selected-z");
+        picked.length = 0;  //reset array
         for (let i = 0; i < seats.length; i++) {
             picked[i] = seats[i].getAttribute("data-value") + " ";
         }
+        
         pickedSeat.innerText = picked.toString();
-
+        calculateTotalPrice();
 
     });
 
-// movieCard.addEventListener("click", turnToBookingPage())
-// function turnToBookingPage(){
-//     alert("Do you want to buy ticket for this movie?");
-// }
+function calculateTotalPrice() {
+    let moviePrice = selectMovie.options[selectMovie.selectedIndex].value;
+    totalPrice.innerText = picked.length * moviePrice;
+}
 
+function toBooking(){
+    let x = confirm("Do you want to buy ticket for this movie?");
+    if(x == true){
+        window.location.href="BookTicket.html";
+    }
+}
+
+let form = document.querySelector('form');
+const email = document.getElementById("input-email-z");
+
+// Add a submit event listener to the form
+form.addEventListener('submit', (event) => {
+
+    let regex = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z]){2,7}$/;
+    let s = email.value;
+
+    if (!regex.test(s)) {
+        event.preventDefault();
+        alert("Invalid Email");
+
+    }else if(picked.length == 0){
+        alert("Please select seats.");
+
+    }else{
+       alert("Booking Success!")
+    }
+
+});
 
 
 
