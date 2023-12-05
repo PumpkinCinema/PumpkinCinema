7a41b2a330837609c6e1d6644729bde88369bb84
Lianne <ideng0212@gmail.com>
2023-12-05 14:59:08 +0000

    style adjust

--- js/main.js
+++ js/main.js
@@ -23,18 +23,6 @@ $(function () {
     });
 });
 
-
-// This JavaScript code uses jQuery to add or remove CSS classes based on certain conditions:
-
-// The checkWidth function checks the width of the window. If the width is less than or equal to 990px, it adds the navbar-dark class to elements with the navbar class. If the width is greater than 990px, it removes the navbar-dark class.
-
-// The checkWidth function is called immediately when the page loads to set the initial state.
-
-// The checkWidth function is also bound to the window's resize event. This means that whenever the window is resized, the function is called again, checking the new width and adding or removing the navbar-dark class as necessary.
-
-// The scroll event of the window is also being listened to. When the window is scrolled and the scroll distance is more than 10, the active class is added to elements with the navbar class. If the scroll distance is less than or equal to 10, the active class is removed.
-
-
 // ------------ About us FORM -------------- @Yinglian Deng------------
 const email = document.getElementById("le-mail");
 const message = document.getElementById("l-message");
